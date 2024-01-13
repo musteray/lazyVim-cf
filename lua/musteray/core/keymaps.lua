fdec97c18561260b3632c7996a89b08bd0099117
Raymund <raymund.balicante@freshclinics.com>
2024-01-13 22:43:56 +0800

    UPDATE: don't show whitespaces/tab/<cr>

--- lua/musteray/core/keymaps.lua
+++ lua/musteray/core/keymaps.lua
@@ -6,8 +6,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
-vim.cmd("set listchars=eol:$,tab:>-,trail:~,extends:>,precedes:<")
-vim.cmd("set list")
+-- vim.cmd("set listchars=eol:$,tab:>-,trail:~,extends:>,precedes:<")
+-- vim.cmd("set list")
 
 ---------------------
 -- General Keymaps -------------------

cb6d88cbb95690d58ea703427fc3d4f78d2d2287
Raymund <raymund.balicante@freshclinics.com>
2024-02-24 23:54:57 +0800

    use default storage in yanky & update plugins

--- lua/musteray/plugins/yanky.lua
+++ lua/musteray/plugins/yanky.lua
@@ -1,10 +1,10 @@
 return {
 	"gbprod/yanky.nvim",
-	dependencies = {
-		{ "kkharji/sqlite.lua" },
-	},
+	-- dependencies = {
+	-- 	{ "kkharji/sqlite.lua" },
+	-- },
 	opts = {
-		ring = { storage = "sqlite" },
+		-- ring = { storage = "sqlite" },
 		picker = {
 			telescope = {
 				mappings = {
